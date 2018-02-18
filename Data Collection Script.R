4e4c48a26fdbde6c16951c980f6d403841d9e872
ppassafiume <mail@ewenharrison.com>
2018-02-18 14:43:20 -0500

    Changed themes to classic to remove gridlines

--- Data Collection Script.R
+++ Data Collection Script.R
@@ -1,4 +1,3 @@
-
 #### Time Series Analysis Group Project ####
 #### Load Libraries ####
 library(cdcfluview)
@@ -9,7 +8,7 @@ library(forecast)
 library(lubridate)
 
 #Set Working directory
-setwd("~/Bellarmine Advanced Analytics/Time Series Forecasting/Time_Series_Project_2018")
+setwd("~/MSA16/Time Series/TimeSeriesProject2018")
 
 #Read in flu data
 flu.data <- read.csv("FluCasesReported.csv")
@@ -32,27 +31,30 @@ flu.ts
 
 # Plot All cases reported for flu data
 autoplot(flu.ts[,3]) +
-      ggtitle("Flu Cases per Week") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Flu Cases per Week") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 # Plot Cases of A against B
 autoplot(flu.ts[,c(1:2)]) +
-      ggtitle("Flu Cases A and B per Week") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Flu Cases A and B per Week") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 
 # Plot onto a polar plot
 ggseasonplot(flu.ts[ ,3], polar = TRUE) +
-      ylab("Flu Cases")+
-      ggtitle("Flu Cases by Week")
+        ylab("Flu Cases")+
+        ggtitle("Flu Cases by Week")
 
 
 # Plot onto a subseries plot
 ggsubseriesplot(flu.ts[ ,3], polar = TRUE) +
-      ylab("Flu Cases")+
-      ggtitle("Flu Cases by Week")
+        ylab("Flu Cases")+
+        ggtitle("Flu Cases by Week") + 
+        theme_classic()
 
 ## Decompose the time series
 flu_decomp <- decompose(flu.ts[,3], type = "multiplicative")
@@ -69,9 +71,10 @@ flu.h1n1 <- ts(flu.data[,c(11,15,19,20,22)], start = 2009, frequency = 52)
 
 # Plot cases of H1N1 compared to total flu cases
 autoplot(flu.h1n1[,c(1,4)]) +
-      ggtitle("Strain AH1N1 Flu Cases per Week") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Strain AH1N1 Flu Cases per Week") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 
 # Looking at Flu A Strains compared to overall flu cases
@@ -79,56 +82,65 @@ flu.typea <- ts(flu.data[,c(10:14,19,20,22)], start = 2009, frequency = 52)
 
 # Plot cases of H1N1 compared to total flu cases
 autoplot(flu.typea[,c(1:5,7)]) +
-      ggtitle("Flu A Cases per Week Compared to All") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Flu A Cases per Week Compared to All") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 # Looking at Flu B Strains compared to overall flu cases
 flu.typeb <- ts(flu.data[,c(16:18,19,20,22)], start = 2009, frequency = 52)
 
 # Plot cases of H1N1 compared to total flu cases
 autoplot(flu.typeb[,c(1:3,5)]) +
-      ggtitle("Flu B Cases per Week Compared to All") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Flu B Cases per Week Compared to All") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 
 # Tests for the flu compared to flu cases
 flu.test <- ts(flu.data[,c(6,17,20:22)], start = 2009, frequency = 52)
 
-# Plot cases of H1N1 compared to total flu cases
+# Plot cases of H1N1 compared to total flu cases....changed theme
 autoplot(flu.test[,c(1:2)]) +
-      ggtitle("Processed Flu Tests and Cases") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Processed Flu Tests and Cases") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 autoplot(flu.test[,3])+
-      ggtitle("Processed Flu Tests and Cases") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Processed Flu Tests and Cases") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 autoplot(flu.test[,3:4])+
-      ggtitle("Processed Flu Tests and Cases Comparing A vs. Total") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Processed Flu Tests and Cases Comparing A vs. Total") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 autoplot(flu.test[,3:5])+
-      ggtitle("Processed Flu Tests and Cases Comparing B vs. Total") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Processed Flu Tests and Cases Comparing B vs. Total") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 autoplot(flu.test[,4:5])+
-      ggtitle("Processed Flu Tests and Cases Comparing A vs. B") +
-      xlab("Weeks, 2009-present") +
-      ylab("Flu Cases")
+        ggtitle("Processed Flu Tests and Cases Comparing A vs. B") +
+        xlab("Weeks, 2009-present") +
+        ylab("Flu Cases") + 
+        theme_classic()
 
 
 ## Create a Forcast Model with Flu A
 fitA <- stlf(flu.ts[,1])
 plot(fit)
+
 ## Forcast for Flu B
 fitB <- stlf(flu.ts[,2]) 
 plot(fitB)
+
 # Forcast for all Flu
 fitall <- stlf(flu.ts[,3])
 plot(fitall)
